e48a26c3f8a4cbd9a6da858034dcc1fba9d93c8f
KirbyNx64 <estelarkirby@gmail.com>
2024-10-07 12:20:27 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ TOKEN = '7835243662:AAEIrLZ5QTSgF6ycJxD9wTuXaLC35nhFhmc'
 API_KEY = 'AIzaSyBEq9n-_Ksm0L069hxM21fJWNN8EjFGKH8'
 
 import google.generativeai as ai
-
 ai.configure(api_key=API_KEY)
 def obtener_chat():
     model = ai.GenerativeModel("gemini-1.5-flash")
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from commands import application
 
 if __name__ == "__main__":
-    print("\n############################")
-    print("    Bocchis Bot (Gemini)")
-    print("############################")
-    print("\n- Iniciando bot...")
-
     application.run_polling()
